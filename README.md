5754cdc4872547214f01de9c6a1da28a9b7636fe
tlucass <rm97913@fiap.com.br>
2023-11-07 10:51:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,207 @@
+# Desenvolvimento da arquitetura de uma aplicação IoT para Eficiência Energética: 
+
+A empresa de eficiência energética Akona Energy adotou uma arquitetura de aplicação IoT (Internet of Things) para otimizar o consumo de energia de seus clientes. Nesse contexto, o código fornecido desempenha um papel importante em coletar, analisar e enviar dados ambientais relevantes. O projeto detalha-se da seguinte forma:
+
+💡 **Coleta de Dados Ambientais:**
+
+- O código utiliza sensores para coletar informações ambientais, incluindo luminosidade, umidade e temperatura.
+- Um sensor de luz (LDR) é usado para medir a luminosidade ambiente.
+- O sensor DHT11 é empregado para medir a temperatura e a umidade do ambiente.
+
+🚶‍♂️ **Sensor de Presença:**
+
+- Um sensor de presença (PIR) é utilizado para detectar movimentos no ambiente.
+
+⚙ **Controle de Dispositivos:**
+- O projeto permite controlar dispositivos, como LEDs, com base nas condições ambientais e no sensor de presença.
+- A lógica do código acende o LED quando a luminosidade é baixa ou o sensor de presença detecta movimento.
+
+🔊 **Comunicação MQTT:**
+
+- O código estabelece uma conexão MQTT para enviar e receber dados.
+- Os dados de luminosidade, temperatura e umidade são publicados em tópicos MQTT específicos para que possam ser monitorados e analisados remotamente.
+
+📈 **Climatização Inteligente:**
+
+- Com base nas leituras de temperatura e umidade, o projeto apresenta uma lógica para controlar um sistema de climatização.
+- Quando a temperatura é alta e a umidade é adequada, o sistema de climatização é ligado, simulado pelo movimento de um servo motor.
+
+⚡ **Monitoramento e Eficiência Energética:**
+
+- Os dados coletados pelos sensores podem ser usados para otimizar o consumo de energia.
+- A empresa Akona Energy pode usar esses dados para tomar decisões informadas sobre iluminação, climatização e outros dispositivos, melhorando a eficiência energética de seus clientes.
+
+## ✅ Arquitetura IoT: 
+<br>
+<p align="center">
+  <img src="./assets/draft.png" width="400">
+</p>
+
+
+## 🔗 Camada de Aplicação(Front-End):
+
+<br>
+
+<p align="center">
+  <img src="./assets/image-2.png" width="150">
+</p>
+
+<br>
+
+O projeto utilizará uma plataforma de dashboard chamada "Freeboard" como a base da camada de front-end. O Freeboard será responsável por exibir os dados coletados por meio de widgets e gráficos. Isso garantirá que as informações sejam apresentadas de maneira fácil de entender e intuitiva para os usuários.
+
+## 🔗 Camada de Back-End:
+
+
+<br>
+
+<p align="center">
+  <img src="./assets/image-5.png" width="300">
+</p>
+
+<br>
+
+
+O projeto fará uso da plataforma Fiware como seu componente de back-end. O Fiware é uma plataforma de código aberto que oferece uma estrutura aberta e padronizada para o desenvolvimento de soluções inteligentes baseadas em IoT. Nesse contexto, o Fiware desempenhará um papel fundamental na coleta, transporte e processamento dos dados provenientes dos sensores.
+
+Esses dados serão encaminhados para um painel de controle (a camada de front-end) onde serão analisados e monitorados. O objetivo principal é garantir a otimização do consumo de energia e manter as condições adequadas de clima, possibilitando o controle eficiente dessas informações.
+
+Essa plataforma Fiware proporciona a base tecnológica necessária para a construção de uma solução completa e integrada, permitindo a gestão inteligente dos dados coletados e fornecendo uma experiência aprimorada aos usuários finais.
+
+---
+
+A camada de back-end abriga componentes essenciais para a coleta, armazenamento e gerenciamento de dados. Os principais componentes incluem:
+
+**Orion Context Broker:**
+- O Orion Context Broker gerencia as entidades e o contexto dos dispositivos IoT, garantindo coleta 	e atualização de dados em tempo real.
+
+**STH-Comet:**
+- O STH-Comet é responsável pelo armazenamento de dados históricos, incluindo informações coletadas pelos sensores ao longo do tempo, permitindo análises retrospectivas e detecção de 	tendências. 
+
+**IoT Agent MQTT:**
+- Este agente IoT estabelece a comunicação eficaz entre os dispositivos IoT e a plataforma IoT, garantindo a transmissão confiável de dados.
+
+**Banco de Dados NoSQL MongoDB:**
+- O MongoDB armazena entidades, registros, subscrições e dados históricos, oferecendo 	escalabilidade e eficiência na gestão de dados.
+
+**Eclipse-Mosquitto:**
+- Um broker MQTT essencial para gerenciar a comunicação bidirecional entre os dispositivos de IoT e a aplicação.
+
+A interação entre essas camadas é crucial para o funcionamento eficiente da aplicação IoT. Os dados são processados pela camada de back-end, armazenados no MongoDB e disponibilizados para análise por meio da camada de aplicação.
+
+## 🔗 Camada de IoT:
+
+<br>
+
+1. **ESP32:** O ESP32 é um microcontrolador de baixo consumo de energia amplamente usado em projetos de IoT. Ele atua como o cérebro do dispositivo IoT, executando programas que controlam a coleta de dados, a comunicação e outras tarefas. O ESP32 é responsável por conectar-se à rede (Wi-Fi, Bluetooth, ou outras tecnologias), coletar dados do sensor e enviar esses dados para a camada de back-end.
+
+<br>
+
+<p align="center">
+  <img src="./assets/image-4.png" width="150">
+</p>
+
+<br>
+
+2. **Sensor LDR (Light Dependent Resistor):** O sensor LDR é um componente que detecta a intensidade da luz no ambiente. Ele é um dos dispositivos sensores comumente usados em projetos IoT para medir a luminosidade. 
+
+<br>
+
+<p align="center">
+  <img src="./assets/image-3.png" width="200">
+</p>
+
+
+3. **Sensor DHT:** Um sensor DHT é um dispositivo que mede a temperatura e a umidade do ambiente e fornece leituras digitais ou analógicas. É amplamente usado em projetos de IoT e automação residencial devido à sua facilidade de uso e baixo custo.
+
+<br>
+
+<p align="center">
+  <img src="./assets/DHT.png" width="200">
+</p>
+
+4. **LED:**  Componente eletrônico semicondutor que emite luz quando uma corrente elétrica passa por ele.
+
+<br>
+
+<p align="center">
+  <img src="./assets/LED.png" width="200">
+</p>
+
+5. **Sensor PIR:** É um dispositivo que detecta movimento de objetos que emitem calor no espectro infravermelho. Esses sensores são comumente usados para detectar a presença de pessoas ou animais em ambientes internos e externos. 
+
+<br>
+
+<p align="center">
+  <img src="./assets/PIR.png" width="200">
+</p>
+
+5. **Servo Motor:** Um servo motor é um dispositivo eletromecânico que converte um sinal de controle em um movimento preciso. Eles são usados em uma variedade de aplicações, desde robótica até sistemas de controle de posição e velocidade. 
+
+<br>
+
+<p align="center">
+  <img src="./assets/servo.png" width="200">
+</p>
+
+---
+
+## 🔗 Instruções de Uso:
+
+Para utilizar o código fornecido e implementar o projeto de eficiência energética da Akona Energy, siga as seguintes etapas:
+
+**1. Componentes Necessários**
+
+- ESP32 (ou outro microcontrolador compatível)
+- Sensor LDR (Light Dependent Resistor)
+- Sensor DHT11 (ou outro sensor de temperatura e umidade)
+- Sensor PIR (Sensor de Presença)
+- LED
+- Servo Motor
+- Conexão Wi-Fi
+- Plataforma IoT (como Fiware, para a camada de back-end)
+
+**2. Configuração do Ambiente de Desenvolvimento:**
+
+- Certifique-se de que você tenha um ambiente de desenvolvimento configurado para programar o ESP32, como o Arduino IDE ou a plataforma de desenvolvimento ESP-IDF.
+
+**3. Conexão de Hardware:**
+
+- Conecte os componentes ao ESP32 conforme as pinagens definidas no código. Isso inclui o sensor LDR, o sensor DHT, o sensor PIR, o LED e o servo motor. Verifique os pinos definidos no código e faça as conexões corretamente.
+
+**4. Configuração de Rede Wi-Fi:**
+
+- Altere as constantes SSID e PASSWORD para corresponder às configurações da sua rede Wi-Fi, para que o ESP32 possa se conectar à rede.
+
+**5. Configuração MQTT:**
+
+- Defina o endereço do Broker MQTT e a porta na variável BROKER_PORT.
+
+**6. Carregando o Código:**
+
+- Compile e carregue o código para o ESP32 usando o ambiente de desenvolvimento escolhido.
+
+**7. Monitoramento e Controle:**
+
+- Após carregar o código, o ESP32 começará a coletar dados ambientais (luminosidade, temperatura e umidade) e detectar movimento com o sensor PIR.
+- Os dados coletados serão enviados via MQTT para o Broker MQTT definido.
+- O código também controla a iluminação (LED) com base nas condições de luminosidade e detecção de movimento.
+- Um servo motor é controlado com base nas leituras de temperatura e umidade, simulando um sistema de climatização.
+
+**8. Monitoramento Remoto:**
+
+- Você pode configurar um aplicativo ou plataforma IoT para receber os dados publicados nos tópicos MQTT correspondentes.
+- Os dados coletados podem ser usados para análise e otimização do consumo de energia.
+
+**9. Eficiência Energética:**
+
+- Use os dados coletados para tomar decisões informadas sobre o uso de dispositivos, como iluminação e sistemas de climatização, a fim de melhorar a eficiência energética.
+
+<br>
+
+## 🔗 Dependências:
+<br>
+
+**Conectividade de Rede:**
+
+- Dependência de uma infraestrutura de rede Wi-Fi confiável para a comunicação com a camada de back-end.
---
+++ cod.ino
@@ -0,0 +1,396 @@
+[Yesterday 8:37 AM] Lucas Palamartschuk de Toledo
+//Autor: Fábio Henrique Cabrini
+//Resumo: Esse programa possibilita ligar e desligar o led onboard, além de mandar o status para o Broker MQTT possibilitando o Helix saber
+//se o led está ligado ou desligado.
+//Revisões:
+//Rev1: 26-08-2023 Código portado para o ESP32 e para realizar a leitura de luminosidade e publicar o valor em um tópico aprorpiado do broker 
+//Autor Rev1: Lucas Demetrius Augusto 
+//Rev2: 28-08-2023 Ajustes para o funcionamento no FIWARE Descomplicado
+//Autor Rev2: Fábio Henrique Cabrini
+
+#include <WiFi.h>
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClient
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <ESP32Servo.h>
+
+Servo myServo;
+ 
+//defines:
+
+//defines de id mqtt e tópicos para publicação e subscribe denominado TEF(Telemetria e Monitoramento de Equipamentos)
+
+#define TOPICO_SUBSCRIBE    "/TEF/akona003/cmd"        //tópico MQTT de escuta
+#define TOPICO_PUBLISH      "/TEF/akona003/attrs"      //tópico MQTT de envio de informações para Broker
+#define TOPICO_PUBLISH_2    "/TEF/akona003/attrs/l"    //tópico MQTT de envio de informações para Broker
+#define TOPICO_PUBLISH_3    "/TEF/akona003/attrs/t"
+#define TOPICO_PUBLISH_4    "/TEF/akona003/attrs/h"
+
+#define ID_MQTT  "fiware_akona3"      //id mqtt (para identificação de sessão)
+
+const int pirPin = 35;  // Pino de entrada do sensor de presença PIR
+const int ldrPin = 2;   // Pino de entrada do sensor de luz (LDR)
+int servoPin = 18;
+int ledPin = 4;  
+const float gama = 0.7;
+const float rl10 = 50;
+
+#define DHTPIN 15           // Pino de dados do sensor DHT11
+#define DHTTYPE DHT11       // Tipo do sensor DHT (DHT11)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// WIFI
+
+const char* SSID = "FIAP-IBM"; // SSID / nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = "Challenge@23!"; // Senha da rede WI-FI que deseja se conectar
+
+// MQTT
+
+const char* BROKER_MQTT = "46.17.108.113"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883; // Porta do Broker MQTT
+int D4 = 2;
+
+//Variáveis e objetos globais
+
+WiFiClient espClient; // Cria o objeto espClient
+PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
+char EstadoSaida = '0';  //variável que armazena o estado atual da saída
+
+//Prototypes
+
+void initSerial();
+void initWiFi();
+void initMQTT();
+void reconectWiFi(); 
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void VerificaConexoesWiFIEMQTT(void);
+void InitOutput(void);
+
+/* 
+
+*  Implementações das funções
+
+*/
+
+void setup() {
+
+    Serial.begin(115200); // Inicializa a comunicação serial
+
+    //inicializações:
+
+    InitOutput();
+    initSerial();
+    initWiFi();
+    initMQTT();
+    pinMode(pirPin, INPUT);
+    pinMode(ldrPin, INPUT);
+    pinMode(ledPin, OUTPUT);  
+
+    myServo.attach(servoPin);
+    myServo.write(90);
+    dht.begin();
+    delay(5000);
+    MQTT.publish(TOPICO_PUBLISH, "s|on");
+
+}
+
+//Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial 
+//        o que está acontecendo.
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void initSerial() {
+
+  Serial.begin(115200);
+
+}
+
+//Função: inicializa e conecta-se na rede WI-FI desejada
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void initWiFi() {
+    delay(10);
+    Serial.println("------Conexao WI-FI------");
+    Serial.print("Conectando-se na rede: ");
+    Serial.println(SSID);
+    Serial.println("Aguarde");
+    reconectWiFi();
+}
+
+//Função: inicializa parâmetros de conexão MQTT(endereço do 
+//        broker, porta e seta função de callback)
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void initMQTT() {
+    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+    MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+}
+
+//Função: função de callback 
+//        esta função é chamada toda vez que uma informação de 
+//        um dos tópicos subescritos chega)
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) 
+
+{
+
+    String msg;
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) {
+
+       char c = (char)payload[i];
+       msg += c;
+
+    }
+
+    Serial.print("- Mensagem recebida: ");
+    Serial.println(msg);
+
+    //toma ação dependendo da string recebida:
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+
+    if (msg.equals("akona003@on|")) {
+
+        digitalWrite(D4, HIGH);
+        EstadoSaida = '1';
+
+    }
+
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+
+    if(msg.equals("akona003@off|")){
+
+        digitalWrite(D4, LOW);
+        EstadoSaida = '0';
+
+    }
+
+}
+
+//Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
+//        em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void reconnectMQTT(){
+
+    while (!MQTT.connected()){
+
+        Serial.print("* Tentando se conectar ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+
+        if (MQTT.connect(ID_MQTT)){
+
+            Serial.println("Conectado com sucesso ao broker MQTT!");
+            MQTT.subscribe(TOPICO_SUBSCRIBE); 
+
+        } 
+
+        else{
+
+            Serial.println("Falha ao reconectar no broker.");
+            Serial.println("Havera nova tentatica de conexao em 2s");
+            delay(2000);
+
+        }
+
+    }
+
+}
+
+//Função: reconecta-se ao WiFi
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void reconectWiFi() {
+
+    //se já está conectado a rede WI-FI, nada é feito. 
+    //Caso contrário, são efetuadas tentativas de conexão
+
+    if (WiFi.status() == WL_CONNECTED)
+
+        return;
+
+    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+
+    while (WiFi.status() != WL_CONNECTED){
+
+        delay(100);
+        Serial.print(".");
+
+    }
+
+    Serial.println();
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(SSID);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+
+}
+
+//Função: verifica o estado das conexões WiFI e ao broker MQTT. 
+//        Em caso de desconexão (qualquer uma das duas), a conexão
+//        é refeita.
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void VerificaConexoesWiFIEMQTT(void){
+
+    if (!MQTT.connected()) 
+
+        reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+
+     reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+
+}
+
+//Função: envia ao Broker o estado atual do output 
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void EnviaEstadoOutputMQTT(void){
+
+    if (EstadoSaida == '1'){
+
+      MQTT.publish(TOPICO_PUBLISH, "s|on");
+      Serial.println("- Led Ligado");
+
+    }
+
+    if (EstadoSaida == '0'){
+
+      MQTT.publish(TOPICO_PUBLISH, "s|off");
+      Serial.println("- Led Desligado");
+
+    }
+
+    Serial.println("- Estado do LED onboard enviado ao broker!");
+    delay(1000);
+
+}
+
+//Função: inicializa o output em nível lógico baixo
+//Parâmetros: nenhum
+//Retorno: nenhum
+
+void InitOutput(void){
+
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+
+    pinMode(D4, OUTPUT);
+    digitalWrite(D4, HIGH);
+    boolean toggle = false;
+
+    for(int i = 0; i <= 10; i++){
+
+        toggle = !toggle;
+        digitalWrite(D4,toggle);
+        delay(200);           
+
+    }
+
+}
+ 
+//programa principal
+
+void loop() {   
+
+  char msgBuffer[6];
+  char luminosityBuffer[6];
+  char temperatureBuffer[6];
+  char humidityBuffer[6];
+
+  //garante funcionamento das conexões WiFi e ao broker MQTT
+
+  VerificaConexoesWiFIEMQTT();
+
+  //envia o status de todos os outputs para o Broker no protocolo esperado
+
+  EnviaEstadoOutputMQTT();
+
+
+  //  // Leitura do sensor de presença
+  //  int pirValue = digitalRead(pirPin);
+  //  dtostrf(pirValue, 4, 2, msgBuffer);
+  //  MQTT.publish(TOPICO_PUBLISH_2,msgBuffer);
+  // Leitura do sensor de luz (LDR) e mapeamento para a faixa de 0 a 100
+
+  int ldrValue = analogRead(ldrPin);
+  int ldrMappedValue = map(ldrValue, 4095, 0, 1024, 0);
+  float voltage = ldrMappedValue / 1024.*5;
+  float resistance = 2000 * voltage / (1-voltage/5);
+  float brightness = pow(rl10*1e3*pow(10,gama)/resistance,(1/gama));
+
+  Serial.print("Sensor de Luz (LDR): ");
+  Serial.println(brightness);
+
+  dtostrf(brightness, 4, 2, luminosityBuffer);
+
+  MQTT.publish(TOPICO_PUBLISH_2, luminosityBuffer);
+ 
+  // Controle do LED com base na luminosidade e presença
+
+  if (brightness > 50 || digitalRead(pirPin) == HIGH) {
+
+    digitalWrite(ledPin, HIGH); // Acende o LED
+
+  }else{
+
+    digitalWrite(ledPin, LOW); // Apaga o LED
+
+  }
+ 
+ 
+  // Leitura da temperatura e umidade
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  Serial.print("Temperatura: ");
+  Serial.println(temperature);
+  Serial.print("Umidade: ");
+  Serial.println(humidity);
+
+  dtostrf(temperature, 4, 2, temperatureBuffer);
+  MQTT.publish(TOPICO_PUBLISH_3, temperatureBuffer);
+  dtostrf(humidity, 4, 2, humidityBuffer);
+  MQTT.publish(TOPICO_PUBLISH_4, humidityBuffer);
+ 
+  // Lógica de controle com base na temperatura e umidade
+    if (temperature > 20 && humidity > 30){
+        Serial.println("Ligando o sistema de climatização...");
+        // Girar o servo motor de 0 a 180 graus
+
+        for (int pos = 0; pos <= 180; pos += 1){
+            myServo.write(pos);
+            delay(3);  // Ajuste a velocidade conforme necessário
+        }
+
+        // Girar o servo motor de 180 a 0 graus
+        for (int pos = 180; pos >= 0; pos -= 1) {
+            myServo.write(pos);
+            delay(3);  // Ajuste a velocidade conforme necessário
+        }
+
+    } else {
+
+        // Desligar o sistema de climatização ou realizar outra ação
+        Serial.println("Desligando o sistema de climatização...");
+
+    }
+ 
+  delay(1000);  // Leituras a cada segundo
+
+  //keep-alive da comunicação com broker MQTT
+
+  MQTT.loop();
+
+}
